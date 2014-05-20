f7ed4d71aa2250a4995dde932514086eadcc2b66
winlandiano <winlandiano@gmail.com>
2014-05-20 21:57:28 +0800

    nothing, daily commit

---
+++ Android/Qwe/src/com/example/Qwe/MainActivity2.java
@@ -0,0 +1,38 @@
+package com.example.Qwe;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity2 extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main_activity2);
+		b1 = (Button)findViewById(R.id.button2);
+		b1.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View arg0) {
+				// TODO Auto-generated method stub
+				Intent intent1 = new Intent(MainActivity2.this,MainActivity.class);
+				startActivity(intent1);
+				MainActivity2.this.finish();
+			}
+		});
+		
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.main_activity2, menu);
+		return true;
+	}
+Button b1=null;
+}
