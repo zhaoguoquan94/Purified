0806379813e2bb5e6c2fe9e5650310ecaa732ae5
GKuChan <gkuchan_root@outlook.com>
2014-07-07 23:29:46 +0800

    update dev

--- Extension/js/background.js
+++ Extension/js/background.js
@@ -1,6 +1,11 @@
 chrome.contextMenus.create({
     "type" : "normal",
     "title" : "选择元素加入Purified",
-    "id" : "purified",
-    "contexts" : ["all"]
+    "contexts" : ["all"],
+    "onclick" : add_to_purified
 });
+
+function add_to_purified(info, tab){
+    alert(123);
+    if ($) alert(3333333);
+}
--- Extension/js/main.js
+++ Extension/js/main.js
@@ -1,14 +1,1 @@
-chrome.contextMenus.update({
-    "id" : "purified",
-    "onclick" : add_to_purified
-});
-
-function add_to_purified(info, tab){
-    alert(123);
-    alert(2222222);
-    if ($) {
-        alert("yyy");
-    } else {
-        alert("nnn");
-    }
-}
+jQuery("a[name=tj_baike]").hide();
